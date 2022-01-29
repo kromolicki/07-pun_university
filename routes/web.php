5f63786dd9b96b76bc387e88a0c99f62ffada985
Kromolicki <laravel@gmail.com>
2022-01-30 00:12:39 +0530

    to upload directory to server_1

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', [StudentAuthController::class, 'index'])->name('login');
 
 /*STUDENT PANEL START*/
 
